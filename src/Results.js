83c6c68122aa61f68d8c037a986a4c98801a84ce
David Jonathan <davejona@yahoo.com>
2020-08-30 11:24:25 +0100

    Result Render
    Rendered Results for Dispaly

--- src/Results.js
+++ src/Results.js
@@ -2,25 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./Results.css";
 import VideoCard from "./VideoCard"
 import axios from "./axios";
-import requests from "./requests";
+// import FlipMove from "./react-flip-move";
 
-function Results() {
+function Results( {selectedOption}) {
     const [movies, setMovies] = useState([]);
 
         useEffect(() =>{
             async function fetchData(){
-                const request = await axios.get(requests.fetchActionMovies);
+                const request = await axios.get(selectedOption);
                     setMovies(request.data.results);
                     return request;
                     // console.log(request);
             }
             fetchData();
-        },[]);
+        },[selectedOption]);
 
     return (
         <div className="results">
             {movies.map(movie => ( 
-            <VideoCard movie={movie} />
+            <VideoCard key={movie.id} movie={movie} />
             ))}
         </div>
     );
